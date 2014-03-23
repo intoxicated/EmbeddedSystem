62f4c10dd486364ec8fc289f04174fa516a8f022
ameske <ameske@dukes.jmu.edu>
2014-03-23 14:40:41 -0400

    Slight structure changes. Edited comments for report.

--- ArduinoSketch/Controller/Controller.ino
+++ ArduinoSketch/Controller/Controller.ino
@@ -1,34 +1,55 @@
-/**
- * Controller -- lock and light
- *
- */
-
 #include <AESLib.h>
-#include <structs.h>
 #include <HashMap.h>
 #include <CRC16.h>
+#include "structs.h"
 
 #define CTRL_ID   4321
 #define MAX_NODES 10
 
-const byte key[] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15}; 
+/**
+ * Controller
+ *
+ * The controller is in charge of responding to requests from sensor nodes,
+ * sanity checking their requests and carrying out the requests if they are
+ * valid. It then reports back to the sensor that the request was performed.
+ *
+ * Acknowledgements:
+ * - The AES library used can be found at https://github.com/DavyLandman/AESlib
+ * - The CRC16 library was written by Tim W. Shilling (www.ShillingSystems.com)
+ *
+ * Last Revision: March 23, 2014
+ */
 
-HashType<int, long> nodeArray[MAX_NODES];
-HashMap<int, long> nodeMap = HashMap<int, long>(nodeArray, MAX_NODES);
-
-byte outgoing[16];
-byte incoming[16];
-
+// Input pins
 const int doorLockLED = 12;
 const int doorUnlockLED = 11;
 const int lightRedLED = 10;
 const int lightGreenLED = 9;
 const int lightBlueLED = 8;
 
-int lockState = 0, lightState = 0;
+//key has to be 16 bytes
+//this key should be set up both shared with controller
+//in case of multiple sensors, controller may need to maintain
+//multiple keys that maps to right sensor
+const byte key[] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15}; 
+
+// A hashmap to set up the known nodes and their current sequence number
+HashType<int, long> nodeArray[MAX_NODES];
+HashMap<int, long> nodeMap = HashMap<int, long>(nodeArray, MAX_NODES);
+
+// Buffers for incoming and outgoing messages
+byte outgoing[16];
+byte incoming[16];
+
+// State information
+int lockState = 0;
+int lightState = 0;
 int res, nodeCount = 0;
 long currentSeq = 0;
 
+/**
+* Sets up the pin mode and the serial port
+*/
 void setup()
 {
    Serial.begin(9600);
@@ -41,6 +62,14 @@ void setup()
    
 }
 
+/**
+* Sends a response back to a sensor node based on the results of the
+* action requested.
+*
+* @param response    The response (success or failure)
+* @param lockState   The lock state after the requested action
+* @param lightState  The light state after the requested action
+*/
 void sendResponse(int response, int lockState, int lightState)
 {
     res_msg msg;
@@ -58,29 +87,49 @@ void sendResponse(int response, int lockState, int lightState)
     Serial.write((byte *)&msg, 16);
 }
 
+/**
+* Checks a message received from a sensor for errors.
+*/
 int checkErrors(data_msg * data)
 {
+   // We somehow have more than the max number of allowed nodes
    if(nodeCount >= MAX_NODES)
        return -1;       
+       
+   // A node is new but is trying to claim that it isn't
    if(nodeMap.getIndexOf(data->id) == -1 && data->sequence != 1)
        return UNODE_ERR;
+   
+   // The sequence number is not correct for a given node
    if(data->sequence < nodeMap.getValueOf(data->id))
        return SEQ_ERR;
+   if(data->sequence == nodeMap.getValueOf(data->id))
+       return DUP_ERR;
+       
+   // A bogus value is in the lock or light request field    
    if(data->lockReq != SIG_LOCK && data->lockReq != SIG_UNLOCK)
        return LOCK_ERR;
    if(data->lightReq != SIG_LIGHTON && data->lightReq != SIG_LIGHTOFF)
        return LIGHT_ERR;
+       
+   // The checksum of the message is not valid.
    if(CRC::CRC16((uint8_t*)data, 8) != data->checksum)
        return CHKSUM_ERR;
-   if(data->sequence == nodeMap.getValueOf(data->id))
-       return DUP_ERR;
+   
    return 0;
 }
 
+/**
+* Performs the requested action from the sensor node
+*
+* @param data  A pointer to the message struct received from the sensor
+* @return  The action return code
+*/
 int doAction(data_msg * data)
 {
    if(data->lockReq == lockState && data->lightReq == lightState)
        return SAME_STATE;
+       
    //change status of door and light
    if(data->lockReq == SIG_LOCK &&
        data->lockReq != lockState){
@@ -119,47 +168,59 @@ int doAction(data_msg * data)
    return NO_ERR;
 }
 
+/**
+* Reads in and processes a message from the sensor node
+*/
 int readAndSet()
 {
+   int error;
    int count = 0;
    
-   //read all available data
+   // Read all available data
    while(Serial.available())
    {
-      //error if available data is longer than 16 bytes
+      // It is an error if available data is longer than 16 bytes
       if(count >= 16)
          return LEN_ERR;
       incoming[count++] = Serial.read();
       delay(10);
    }
    
-   //error if collected data is shorter than 16 bytes
+   // It is an error if collected data is shorter than 16 bytes
    if(count != 16)
        return LEN_ERR;
-   int error;
-   //decrypt data
+       
+   // Decrypt the message
    aes128_dec_single(key,incoming); 
    data_msg * data = (data_msg *)incoming;
    
+   // Check that the sequence number is ok.
    currentSeq = data->sequence;
    
+   // Check that there aren't any errors in the message to the client
    if((error = checkErrors(data)) != NO_ERR)
        return error;
-   // first sequence, so save both id and sequnence    
+   
+   // If this is a new node, add it to our list of known nodes. Otherwise
+   // update the sequence number for the given node.  
    if(data->sequence == 1 && nodeCount < 10) {
        nodeMap[nodeCount++](data->id, data->sequence);
    }
+   else {
+     nodeMap.setValueOf(data->id, data->sequence);
+   }
    
-   //otherwise, previously communicated
-   nodeMap.setValueOf(data->id, data->sequence);
-   
-   //change state as request 
+   // Change the state as requested
    if((error = doAction(data)) != NO_ERR)
        return error;
 
    return NO_ERR;
 }
 
+/**
+* The main event loop of the controller node. Simply continue to loop
+* until there is data available.
+*/
 void loop()
 {
    if(Serial.available())
--- ArduinoSketch/Sensor/Sensor.ino
+++ ArduinoSketch/Sensor/Sensor.ino
@@ -1,15 +1,33 @@
-
-/** 
- * Sensor sketch  
- *
- */
-
-//ACElib ref:https://github.com/DavyLandman/AESLib
 #include <AESLib.h>
-#include <structs.h>
 #include <CRC16.h>
+#include "structs.h"
 
 #define NODE_ID   1234
+
+/** 
+ * Sensor
+ *
+ * The sensor is in charge of gathering environmental data, specifically,
+ * motion and lumosity, and reporting to the controller whether or not
+ * the state of the room should be changed.
+ *
+ * The sensor is more aggressive with locking the room than it is with
+ * turning the lights off. Currently, the sensor will send a "lock door"
+ * request after 15 seconds of no movement, and a "turn off lights" requeset
+ * after 120 seconds of no movement. These are arbitrarily set and can be
+ * tweaked based on the requirements of the room.
+ *
+ * Acknowledgements:
+ * - The AES library used can be found at https://github.com/DavyLandman/AESlib
+ * - The CRC16 library was written by Tim W. Shilling (www.ShillingSystems.com)
+ *
+ * Last Revision: March 23, 2014
+ */
+
+// Input pins 
+const int motionOutput = 13;
+const int motionInput = 4; 
+const int lightInput = A5;
 
 //key has to be 16 bytes
 //this key should be set up both shared with controller
@@ -19,17 +37,15 @@ const byte key[] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
 
 byte incoming[16];
 
-//input pins 
-const int motionOutput = 13;
-const int motionInput = 4; 
-const int lightInput = A5;
-
 long seq = 0;
 int isMotion, brightness;
 int lockState = 0, lightState = 0;
 boolean receivedAck = false;
 boolean sentMsg = false;
 
+/**
+* Sets up the pin mode and the serial port
+*/
 void setup()
 {
     Serial.begin(9600);
@@ -37,7 +53,8 @@ void setup()
     pinMode(lightInput, INPUT); 
 }
 
-void motionHasDetected()
+
+void motionDetected()
 {
    digitalWrite(motionOutput, HIGH);
    delay(3000);
@@ -45,10 +62,19 @@ void motionHasDetected()
    delay(1000);  
 }
 
+/**
+* sendData sets up the message from the sensor to the controller
+* by setting the request values, the sequence number, and encrypting
+* the message.
+* 
+* @param lock    1 if we are locking the door, 0 otherwise
+* @param light   1 if we are turning on the lights, 0 otherwise
+*/
 void sendData(int lock, int light)
 {
     data_msg data;
     memset(&data,0x0,16);
+    
     data.id = NODE_ID;
     data.sequence = seq++;
     data.reserve = 0xFF;
@@ -56,29 +82,36 @@ void sendData(int lock, int light)
     data.lightReq = light;
     data.checksum = CRC::CRC16((uint8_t *)&data, 8);
     memset(data.padding, 0x20, 6);
+    
     //encrypt
     aes128_enc_single(key, &data);
+    
     //send
     Serial.write((byte *)&data, 16);
 }
 
+
 /**
- * get called when new data arrive from controller
- * adjust state of light and lock if necessary
- *
+ * serialEvent is called when there is data available in the
+ * buffer in-between runs of the loop function. In our case
+ * this means there is a message for the sensor from the
+ * controller. Handle it.
  */
 void serialEvent()
 {
    int count = 0;
+   
+   // Read in all the available bytes into a buffer
    while(Serial.available())
    {
-      if(count >= 16)
-          return;
+      if(count >= 16)      return;
+      
       incoming[count++] = Serial.read();
       delay(10);
    }
  
-   //only care if 16 bytes of data
+   // We only care if there is exactly 16 bytes of data which
+   // constitutes a full message in our system.
    if(count == 16)
    {
       aes128_dec_single(key, incoming);
@@ -95,19 +128,16 @@ void serialEvent()
       }
       receivedAck = true;
    }
-   else //if not 16 bytes, ignore
-     ;
 }
 
+/**
+* The main event loop of the sensor node.
+*/
 void loop()
 {
-    //receive signal from motion sensor
+    // Gather the environment data for this run of the loop
     isMotion = digitalRead(motionInput);
-    
-    //read brightness
     brightness = analogRead(lightInput);
-    
-    //brightness = constrain(brightness, 970, 1023);
     brightness = map(brightness, 0, 1023, 0, 255);
 
     //reset send/ack flag
@@ -116,8 +146,8 @@ void loop()
     
 sendMessage:  
     if(isMotion){
-       motionHasDetected();
-       if(lockState == SIG_LOCK || lightState == SIG_LIGHTOFF){
+       motionDetected();
+       if(lockState == SIG_LOCK || lightState == SIG_LIGHTOFF) {
          //check brightness here
            sendData(SIG_UNLOCK, SIG_LIGHTON);
            sentMsg = true;
@@ -125,7 +155,7 @@ sendMessage:
     } 
     else 
     {
-       if(lockState == SIG_UNLOCK || lightState == SIG_LIGHTON){
+       if(lockState == SIG_UNLOCK || lightState == SIG_LIGHTON) {
          //check brightness here
            sendData(SIG_LOCK, SIG_LIGHTOFF);
            sentMsg = true; 
